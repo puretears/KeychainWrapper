28eb41f1668ee1a9915734541098ebd74166a6ed
Mars <nacci520@gmail.com>
2019-09-26 18:56:38 +0800

    Add property wrapper support.

---
+++ KeychainWrapper/KeychainItem.swift
@@ -0,0 +1,130 @@
+//
+//  KeychainItem.swift
+//  KeychainWrapper
+//
+//  Created by Mars on 2019/9/22.
+//  Copyright © 2019 Mars. All rights reserved.
+//
+
+import Foundation
+
+/**
+ class Demo {
+   @KeychainStoreString(key: "demo.key") var value: String?
+ }
+ */
+
+@propertyWrapper
+public struct KeychainStoreString {
+  public let key: String
+  public let `default` = KeychainWrapper.default
+  
+  public init(key: String) {
+    self.key = key
+  }
+  
+  public var wrappedValue: String? {
+    get {
+      KeychainWrapper.default.string(forKey: key)
+    }
+    
+    set {
+      guard let v = newValue else { return }
+      
+      KeychainWrapper.default.set(v, forKey: key)
+    }
+  }
+  
+  public var projectedValue: KeychainWrapper {
+    get {
+      return self.default
+    }
+  }
+}
+
+@propertyWrapper
+public struct KeychainStoreNumber<T> where T: Numeric, T: Codable {
+  public let key: String
+  public let `default` = KeychainWrapper.default
+  
+  public init(key: String) {
+    self.key = key
+  }
+  
+  public var wrappedValue: T? {
+    get {
+      KeychainWrapper.default.object(of: T.self, forKey: key)
+    }
+    
+    set {
+      guard let v = newValue else { return }
+      
+      KeychainWrapper.default.set(v, forKey: key)
+    }
+  }
+  
+  public var projectedValue: KeychainWrapper {
+    get {
+      return self.default
+    }
+  }
+}
+
+@propertyWrapper
+public struct KeychainStoreObject<T> where T: Codable {
+  public let key: String
+  public let `default` = KeychainWrapper.default
+  
+  public init(key: String) {
+    self.key = key
+  }
+  
+  public var wrappedValue: T? {
+    get {
+      KeychainWrapper.default.object(of: T.self, forKey: key)
+    }
+    
+    set {
+      guard let v = newValue else { return }
+      
+      KeychainWrapper.default.set(v, forKey: key)
+    }
+  }
+  
+  public var projectedValue: KeychainWrapper {
+    get {
+      return self.default
+    }
+  }
+}
+
+//@propertyWrapper
+//public struct KeychainStore<T> where T: Codable {
+//  public let key: String
+//  public let `default` = KeychainWrapper.default
+//
+//  public init(key: String) {
+//    self.key = key
+//  }
+//
+//  public init(wrappedValue: T, key: String) {
+//    self.init(key: key)
+//    self.wrappedValue = wrappedValue
+//  }
+//
+//  public var wrappedValue: T {
+//    get {
+//      KeychainWrapper.default.object(of: T.self, forKey: key)!
+//    }
+//
+//    set {
+//      KeychainWrapper.default.set(newValue, forKey: key)
+//    }
+//  }
+//
+//  public var projectedValue: KeychainWrapper {
+//    get {
+//      return self.default
+//    }
+//  }
+//}
---
+++ KeychainWrapperTests/KeychainItemTests.swift
@@ -0,0 +1,54 @@
+//
+//  KeychainItemTests.swift
+//  KeychainWrapperTests
+//
+//  Created by Mars on 2019/9/22.
+//  Copyright © 2019 Mars. All rights reserved.
+//
+
+import XCTest
+@testable import KeychainWrapper
+
+class Point: Codable {
+  var x: Double = 0
+  var y: Double = 0
+}
+
+class Demo {
+  @KeychainStoreNumber(key: "int.demo.key") var value: Int
+  @KeychainStoreObject(key: "point.demo.key") var point: Point?
+}
+
+class KeychainItemTests: XCTestCase {
+  override func setUp() {
+      // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+
+  override func tearDown() {
+      // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+
+  func testKeychainItemNumber() {
+    let demo = Demo()
+    demo.value = 1110
+    
+    XCTAssertEqual(demo.value, 1110)
+    
+    var valueInKC = KeychainWrapper.default.object(of: Int.self, forKey: "int.demo.key")
+    XCTAssertEqual(valueInKC, 1110)
+    
+    demo.value = 1010
+    valueInKC = KeychainWrapper.default.object(of: Int.self, forKey: "int.demo.key")
+    XCTAssertEqual(valueInKC, 1010)
+    
+    valueInKC = KeychainWrapper.default.object(of: Int.self, forKey: "general.int.demo.key")
+    XCTAssertEqual(valueInKC, 11)
+  }
+  
+  func testKeychainItemObject() {
+    let demo = Demo()
+    demo.point = Point()
+    
+    
+  }
+}
